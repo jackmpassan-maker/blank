60c6a112dde10549da4e26d47a61f8d4252c8e61
jackmpassan-maker <jackmpassan@gmail.com>
2025-12-25 23:02:54 -0600

    Update regional multipliers for urban and rural areas

--- engine.py
+++ engine.py
@@ -18,9 +18,9 @@ TIMING_MULTIPLIERS = {
 # Regional multipliers
 # --------------------
 REGION_MULT = {
-    "urban": 0.75,
+    "urban": 0.8,
     "suburban": 1.00,
-    "rural": 1.25,
+    "rural": 1.3,
 }
 
 # --------------------
